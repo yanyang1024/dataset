044dd3c73a85ea63ec52f3791d4632828dd39b0b
yanyang1024 <2201212819@stu.edu.pku.cn>
2022-11-13 21:13:44 +0800

    test

--- down_kine.py
+++ down_kine.py
@@ -9,6 +9,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+print('Done!')
 
 # lines = []
 # keyword_txt = '0_class_cn_0605'
